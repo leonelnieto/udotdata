82d667b6afdab4f25b61b9e7755ba2fe7bf041e8
Emily Sowder <42614651+esowder@users.noreply.github.com>
2018-10-16 16:17:49 -0500

    Correct Column Name: % Year Lapsed to Elapsed

--- maintenence/script.js
+++ maintenence/script.js
@@ -61,7 +61,7 @@ fetch(ExpendutiresData).then(function(response){
 				console.log(Expenditures);
 				console.log(Budget);
 				var content = "<table class='flat-table'><tbody>";
-				content += "<tr><th>Region</th><th>Budget</th><th>Spent</th><th>Difference</th><th>% $ Expended</th><th>% Year Lapsed</th></tr>";
+				content += "<tr><th>Region</th><th>Budget</th><th>Spent</th><th>Difference</th><th>% $ Expended</th><th>% Year Elapsed</th></tr>";
 				for(var i=0; i< Expenditures.length;i++) {
 					difference = Number(Budget[i]["budget"]) - Number(Expenditures[i]["expenditures"]);
 					burned = Math.round((Number(Expenditures[i]["expenditures"])/ Number(Budget[i]["budget"]))*100);
