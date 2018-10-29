5292dfcba6c26c6548e131d9751b2b4947d243f1
esowder <esowder@nd.edu>
2018-10-29 13:29:41 -0500

    removed year extra stuff

--- Right_of_Way/Acquisitions/script.js
+++ Right_of_Way/Acquisitions/script.js
@@ -6,32 +6,6 @@ const formatter = new Intl.NumberFormat('en-US', {
 })
 
 //Define Variables
-
-var today = new Date();
-var year = today.getFullYear();
-var fiscalyear,fisStartDate,fisEndDate;
-if ((today.getMonth() + 1)  <= 6) {
-    //Its before July, print current year as fiscal year
-	fiscalyear = year;
-	//console.log("Fiscal Year:"+fiscalyear);
-	//Set fiscal start date to last year july 1st
-	fisStartDate = new Date(year - 1, 6,1);
-	//console.log("Start Date:"+fisStartDate);
-	//Set fiscal end date to next year June 30th
-	fisEndDate = new Date(year, 5,30);
-	//console.log("End Date:"+fisEndDate);
-  } else {
-	//Its after July, print current year + 1 as fiscal year
-    fiscalyear = today.getFullYear() + 1;
-	//console.log("Fiscal Year:"+fiscalyear);
-	//Set fiscal start date to July st
-	fisStartDate = new Date(year,6,1);
-	//console.log("Start Date:"+fisStartDate);
-	//Set Fiscal End Date to current year july 1st.
-	fisEndDate = new Date(fiscalyear,5,30);
-	//console.log("End Date:"+fisEndDate);
-  }
-
 var domain = 'https://dashboard.udot.utah.gov/resource/';
 var dataset0 = 'ie6m-xuux.json';
 var query0 = '?$SELECT=avg(limitation_days_saved)';
