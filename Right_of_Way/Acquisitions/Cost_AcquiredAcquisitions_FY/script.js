cda8e6515d9b7a564ad043f1816608f24c8a33ad
esowder <esowder@nd.edu>
2018-10-31 14:28:34 -0500

    update style and css

---
+++ Right_of_Way/Acquisitions/Cost_AcquiredAcquisitions_FY/script.js
@@ -0,0 +1,24 @@
+/*Java script file pulls data from socrata table
+and build and populates an html table */
+
+//Define Variables
+var domain = 'https://dashboard.udot.utah.gov/resource/';
+var dataset0 = 'c9s4-gvqp.json';
+var query0 = '?$query=SELECT sum(amount) WHERE fiscal_year=%272018%27 AND account_type=%2722%27 AND expense_type IN ('6970','6971','6972','6987')';
+var RightofWayDataset = domain+dataset0+query0;
+
+(function() {
+  'use strict';
+  fetch(RightofWayDataset)
+    .then(function(response) {
+
+      response.json()
+        .then(function(data) {
+          document.getElementById('indexNum').innerHTML = numberWithCommas(data[0].sum_amount)
+        });
+    })
+})();
+
+function numberWithCommas(x) {
+return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
