9766ea0579e6af9aaa50fbbe0d137d57c8754c92
esowder <esowder@nd.edu>
2018-10-31 14:49:19 -0500

    updated dataset call

--- Right_of_Way/Acquisitions/Cost_AcquiredAcquisitions_FY/script.js
+++ Right_of_Way/Acquisitions/Cost_AcquiredAcquisitions_FY/script.js
@@ -2,13 +2,15 @@
 and build and populates an html table */
 
 //Define Variables
-var domain = 'https://dashboard.udot.utah.gov/resource/';
-var dataset0 = 'c9s4-gvqp.json';
-var query0 = '?$query=SELECT sum(amount) WHERE fiscal_year=%272018%27 AND account_type=%2722%27 AND expense_type IN ('6970','6971','6972','6987')';
-var RightofWayDataset = domain+dataset0+query0;
+//var domain = 'https://dashboard.udot.utah.gov/resource/';
+//var dataset0 = 'c9s4-gvqp.json';
+//var query0 = '?$query=SELECT sum(amount) WHERE fiscal_year=%272018%27 AND account_type=%2722%27 AND expense_type IN ('6970','6971','6972','6987')';
+//var RightofWayDataset = domain+dataset0+query0;
 
 (function() {
   'use strict';
+  var RightofWayDataset = "https://dashboard.udot.utah.gov/resource/c9s4-gvqp.json?$query=SELECT sum(amount) WHERE fiscal_year=%272018%27 AND account_type=%2722%27 AND expense_type IN ('6970','6971','6972','6987')"
+
   fetch(RightofWayDataset)
     .then(function(response) {
 
